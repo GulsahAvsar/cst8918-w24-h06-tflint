819f0ff35f64b922a1fcb7e82b6ab6554fcdd2b2
GulsahAvsar <avsa0005@algonquinlive.com>
2025-02-21 19:55:08 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Define config variables
+variable "labelPrefix" {
+  type        = string
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  type    = string
+  default = "canadacentral"
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureadmin"
+  description = "The username for the local user account on the VM."
+}
